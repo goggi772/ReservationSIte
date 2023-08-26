2b169424e7e32ff3d5d791ac545421a352b99746
SunYub <goggi772@gmail.com>
2023-08-26 17:02:53 +0900

    Initial commit

---
+++ src/main/java/com/reserv/reservationsite/ReservationSIteApplication.java
@@ -0,0 +1,13 @@
+package com.reserv.reservationsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationSIteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationSIteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/reserv/reservationsite/ReservationSIteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reserv.reservationsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationSIteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
